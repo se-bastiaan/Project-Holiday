d540b81d28924062b3eb7da2b71b5938a475bc43
shemels <shemels@student.ru.nl>
2015-05-26 14:59:25 +0200

    Added calculations to get the day type (also added enums to use in calculations).

---
+++ app/src/main/java/nl/teamone/projectholiday/PackingList/SunType.java
@@ -0,0 +1,10 @@
+package nl.teamone.projectholiday.PackingList;
+
+/**
+ * Created by IAmHeavenly on 26-5-2015.
+ */
+public enum SunType {
+    mSun1, //less than 20 degrees celsius
+    mSun2, //20-25 degrees celsius
+    mSun3, //more than 25 degrees celsius
+}
