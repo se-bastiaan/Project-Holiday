6a5d4ef0e206368f071864e5534498f8ae77caf2
Sébastiaan <se_bastiaan@outlook.com>
2015-06-09 01:29:38 +0200

    Made location test possible

--- app/src/main/java/nl/teamone/projectholiday/HolidayApplication.java
+++ app/src/main/java/nl/teamone/projectholiday/HolidayApplication.java
@@ -2,6 +2,8 @@ package nl.teamone.projectholiday;
 
 import android.app.Application;
 
+import timber.log.Timber;
+
 public class HolidayApplication extends Application {
 
     private static HolidayApplication sThis;
@@ -14,6 +16,10 @@ public class HolidayApplication extends Application {
     public void onCreate() {
         super.onCreate();
         sThis = this;
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
     }
 
 }
--- app/src/main/java/nl/teamone/projectholiday/ui/activities/MainActivity.java
+++ app/src/main/java/nl/teamone/projectholiday/ui/activities/MainActivity.java
@@ -3,9 +3,17 @@ package nl.teamone.projectholiday.ui.activities;
 import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.List;
+import java.util.Timer;
+
 import nl.teamone.projectholiday.Preferences;
 import nl.teamone.projectholiday.R;
+import nl.teamone.projectholiday.api.objects.Location;
 import nl.teamone.projectholiday.utils.PrefUtils;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
+import timber.log.Timber;
 
 /**
  * The main activity of the application. If the user has a planned trip, then this activity will show the right fragment
@@ -24,7 +32,20 @@ public class MainActivity extends BaseActivity {
             Intent intent = new Intent(this, ExplanationActivity.class);
             startActivity(intent);
             finish();
+            return;
         }
+
+        getData();
+    }
+
+    private void getData() {
+        Location.find("Vossendijk, Nijmegen").observeOn(AndroidSchedulers.mainThread()).subscribe(new Action1<List<Location>>() {
+            @Override
+            public void call(List<Location> locations) {
+                for (Location loc : locations)
+                    Timber.d("Location: %s", loc.toString());
+            }
+        });
     }
 
 }
