26daf42ea34bef34ad3cccd6ad06291bc96d1ff6
Caethy <r.tonen@gmail.com>
2015-06-09 14:56:06 +0200

    Tiny Changes

--- app/src/main/java/nl/teamone/projectholiday/api/responses/openweathermap/WeatherBlock.java
+++ app/src/main/java/nl/teamone/projectholiday/api/responses/openweathermap/WeatherBlock.java
@@ -5,6 +5,6 @@ public class WeatherBlock {
     public int id;
     public String main;
     public String description;
-    public String icon;
+    public String icon; //http://openweathermap.org/img/w/{STRING}.png
 
 }
