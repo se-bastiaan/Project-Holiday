0c769fb1af3b4848240327451efddedda3d0e9b3
shemels <shemels@student.ru.nl>
2015-06-10 14:43:54 +0200

    think -> thick

---
+++ app/src/main/java/nl/teamone/projectholiday/api/responses/autocompletemap/predictions.java
@@ -0,0 +1,16 @@
+package nl.teamone.projectholiday.api.responses.autocompletemap;
+
+/**
+ * Created by IAmHeavenly on 10-6-2015.
+ */
+public class predictions {
+    String description;
+    String id;
+    matched_substrings matched_substrings;
+    String place_id;
+    String reference;
+    terms terms;
+    String[] types;
+
+
+}
